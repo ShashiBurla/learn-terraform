f347dc68bbae4e774e50d0d9c9eca6277634ca44
ShashiBurla <burlashashidar@gmail.com>
2026-07-21 23:20:24 +0530

    creating servers with modules

--- 05-module/main.tf
+++ 05-module/main.tf
@@ -1,5 +1,25 @@
-module "simple" {
-  for_each = var.components
+module "app_deploy" {
+  depends_on = [ module.db_deploy ]
+  for_each = var.App_components
+  source = "./simple_module"
+  resource_group = var.resource_group
+  component_name = each.key
+  image_id = var.image_id
+  location = var.location
+  size = each.value
+}
+module "db_deploy" {
+  for_each = var.db_components
+  source = "./simple_module"
+  resource_group = var.resource_group
+  component_name = each.key
+  image_id = var.image_id
+  location = var.location
+  size = each.value
+}
+module "ui_deploy" {
+  depends_on = [ module.app_deploy ]
+  for_each = var.ui_components
   source = "./simple_module"
   resource_group = var.resource_group
   component_name = each.key
--- 05-module/variables.tf
+++ 05-module/variables.tf
@@ -8,20 +8,28 @@ variable "image_id" {
   default = "/subscriptions/e890e2a1-2eca-4c0b-8e51-dd98c583dd43/resourceGroups/devops-practiece/providers/Microsoft.Compute/galleries/rhel10.1/images/1.0.0"
 }
 
-variable "components" {
+variable "db_components" {
   default = {
-    frontend     = "Standard_B1s"
     mysql        = "Standard_B1s"
     valkey       = "Standard_B1s"
-    # mongodb      = "Standard_B1s"
-    # rabbitmq     = "Standard_B1s"
-    # catalogue    = "Standard_B1s"
-    # user         = "Standard_B1s"
-    # cart         = "Standard_B1s"
-    # shipping     = "Standard_B1s"
-    # order        = "Standard_B1s"
-    # notification = "Standard_B1s"
-    # ratings      = "Standard_B1s"
-    # payment      = "Standard_B1s"
+    mongodb      = "Standard_B1s"
+    rabbitmq     = "Standard_B1s"
+  }
+}
+variable "App_components" {
+  default = {
+    catalogue    = "Standard_B1s"
+    user         = "Standard_B1s"
+    cart         = "Standard_B1s"
+    shipping     = "Standard_B1s"
+    order        = "Standard_B1s"
+    notification = "Standard_B1s"
+    ratings      = "Standard_B1s"
+    payment      = "Standard_B1s"
+  }
+}
+variable "ui_components" {
+  default = {
+    frontend     = "Standard_B1s"
   }
 }
