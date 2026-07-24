d8b79c5c73b52bdfafb9f016b3c9eace856547b7
ShashiBurla <burlashashidar@gmail.com>
2026-07-24 15:29:04 +0530

    adding git ignore

--- 09-create_VM_for_LB/main.tf
+++ 09-create_VM_for_LB/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   secure_boot_enabled             = true
   vtpm_enabled                    = true
   source_image_id                 = var.image_id
-  
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -36,4 +36,26 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = var.resource_group
   location            = var.location
   allocation_method   = "Static"
+}
+resource "azurerm_network_security_group" "main_nsg" {
+  name                = "main-nsg"
+  location            = "Denmark East"
+  resource_group_name = "devops-practiece"
+
+  # Allow SSH traffic (Port 22)
+  security_rule {
+    name                       = "Allow-SSH-22"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*" # For better security, replace with your specific IP (e.g., "203.0.113.50/32")
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = "/subscriptions/e890e2a1-2eca-4c0b-8e51-dd98c583dd43/resourceGroups/devops-practiece/providers/Microsoft.Network/virtualNetworks/terraform-vnet/subnets/default"
+  network_security_group_id = azurerm_network_security_group.main_nsg.id
 }
